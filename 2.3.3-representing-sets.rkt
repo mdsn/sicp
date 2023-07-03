351006469490d705094eb2071c05c983083cfac2
Mariano <madasbytes@gmail.com>
2023-07-03 01:19:07 -0400

    2.3.3 Sets as ordered lists (2.61, 2.62)

--- 2.3.3-representing-sets.rkt
+++ 2.3.3-representing-sets.rkt
@@ -49,3 +49,56 @@
 
 (adjoin-set 3 '(1 2 3)) ; '(3 1 2 3)
 (union-set '(a b c d) '(c d e f)) ; '(a b c d c d e f)
+
+; sets as ordered lists
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+		((= x (car set)) true)
+		((< x (car set)) false)
+		(else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+	'()
+	(let ((x1 (car set1)) (x2 (car set2)))
+	  (cond ((= x1 x2)
+			 (cons x1
+				   (intersection-set (cdr set1) (cdr set2))))
+			((< x1 x2)
+			 (intersection-set (cdr set1) set2))
+			((< x2 x1)
+			 (intersection-set set1 (cdr set2)))))))
+
+; exercise 2.61
+(define (adjoin-set x set)
+  (if (null? set)
+	(list x)
+	(let ((head (car set)))
+	  (cond ((= x head) set)
+			((> x head)
+			 (cons head (adjoin-set x (cdr set))))
+			(else (cons x set))))))
+
+(adjoin-set 5 '(1 2 3 8 9)) ; '(1 2 3 5 8 9)
+(adjoin-set 8 '(1 2 3 8 9)) ; '(1 2 3 8 9)
+(adjoin-set 3 '()) ; '(3)
+(adjoin-set 10 '(1 2 3 4)) ; '(1 2 3 4 10)
+(adjoin-set 1 '(5 10 15)) ; '(1 5 10 15)
+
+; exercise 2.62
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+		((null? set2) set1)
+		(else
+		  (let ((x1 (car set1)) (x2 (car set2)))
+			(cond ((= x1 x2)
+				   (cons x1 (union-set (cdr set1) (cdr set2))))
+				  ((< x1 x2)
+				   (cons x1 (union-set (cdr set1) set2)))
+				  ((> x1 x2)
+				   (cons x2 (union-set set1 (cdr set2)))))))))
+
+(union-set '(1 2 3 4) '(4 5 7 8)) ; '(1 2 3 4 5 7 8)
+(union-set '(1 2 3) '(1 2 3)) ; '(1 2 3)
+(union-set '() '(1 2 3)) ; '(1 2 3)
+(union-set '(1 2 3) '(2 3 4 5 7 8)) ; '(1 2 3 4 5 7 8)
