bf4503f814759ede4e42c979e3cc4a1a9d724870
Mariano Casco <mariano@mailbox.org>
2025-04-05 18:36:00 -0400

    Code of section 4.1.2, exercise 4.2

--- 4.1-the-metacircular-evaluator.rkt
+++ 4.1-the-metacircular-evaluator.rkt
@@ -59,6 +59,149 @@
                     env)
   'ok)
 
+(define (self-evaluating? exp)
+  (cond ((number? exp) true)
+        ((string? exp) true)
+        (else false)))
+
+(define (variable? exp)
+  (symbol? exp))
+
+(define (quoted? exp)
+  (tagged-list? exp 'quote))
+
+(define (text-of-quotation exp)
+  (cadr exp))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+    (eq? (car exp) tag)
+    false))
+
+(define (assignment? exp)
+  (tagged-list? exp 'set!))
+
+(define (assignment-variable exp)
+  (cadr exp))
+
+(define (assignment-value exp)
+  (caddr exp))
+
+(define (definition? exp)
+  (tagged-list? exp 'define))
+
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+    (cadr exp)      ; (define x 1)
+    (caadr exp)))   ; (define (f x) ...)
+
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+    (caddr exp)
+    (make-lambda (cdadr exp)
+                 (cddr exp))))
+
+(define (lambda? exp)
+  (tagged-list? exp 'lambda))
+
+(define (lambda-parameters exp)
+  (cadr exp))
+
+(define (lambda-body exp)
+  (cddr exp))
+
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+(define (if? exp)
+  (tagged-list? exp 'if))
+
+(define (if-predicate exp)
+  (cadr exp))
+
+(define (if-consequent exp)
+  (caddr exp))
+
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+    (cadddr exp)
+    'false))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
+(define (begin? exp)
+  (tagged-list exp 'begin))
+
+(define (begin-actions exp)
+  (cdr exp))
+
+(define (last-exp? seq)
+  (null? (cdr seq)))
+
+(define (first-exp seq)
+  (car seq))
+
+(define (rest-exps seq)
+  (cdr seq))
+
+(define (sequence->exp seq)
+  (cond ((null? seq) seq)
+        ((last-exp? seq) (first-exp seq))
+        (else (make-begin seq))))
+
+(define (make-begin seq)
+  (cons 'begin seq))
+
+(define (application? exp)
+  (pair? exp))
+
+(define (operator exp)
+  (car exp))
+
+(define (operands exp)
+  (cdr exp))
+
+(define (no-operands? ops)
+  (null? ops))
+
+(define (first-operand ops)
+  (car ops))
+
+(define (rest-operands ops)
+  (cdr ops))
+
+(define (cond? exp)
+  (tagged-list? exp 'cond))
+
+(define (cond-clauses exp)
+  (cdr exp))
+
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+
+(define (cond-predicate clause)
+  (car clause))
+
+(define (cond-actions clause)
+  (cdr clause))
+
+(define (cond->if exp)
+  (expand-clauses (cond-clauses exp)))
+
+(define (expand-clauses clauses)
+  (if (null? clauses)
+    'false
+    (let ((first (car clauses))
+          (rest (cdr clauses)))
+      (if (cond-else-clause? first)
+        (if (null? rest)
+          (sequence->exp (cond-actions first))
+          (error "ELSE clause isn't last -- COND->IF" clauses))
+        (make-if (cond-predicate first)
+                 (sequence->exp (cond-actions first))
+                 (expand-clauses rest))))))
+
 ; 4.1
 (define (list-of-values-l2r exps env)
   (if (no-operands? exps)
@@ -74,5 +217,28 @@
       (begin (set! tail (list-of-values-r2l (rest-operands exps) env))
              (cons (eval (first-operand exps) env)
                    tail)))))
+
+; 4.2
+; Suppose Louis writes eval like this:
+;
+;    (define (eval exp env)
+;      (cond ((application? exp) ...)
+;            ((assignment? exp) ...)
+;            ((definition? exp) ...)
+;            ...
+;
+; Now consider (define x 3). With the definition we have
+; of `application?`, this expression will match this cond
+; clause and attempt to apply `define` to x and 3 instead
+; of pushing a new definition.
+
+; In order for applications to be checked first, something
+; must call them out distinctly from other elements of the
+; language. A 'call list tag will be it:
+;
+;    (define (application? exp)  ; ('call operator <operands>)
+;      (tagged-list? exp 'call))
+;    (define (operator exp) (cadr exp))
+;    (define (operands exp) (cddr exp))
 
 
